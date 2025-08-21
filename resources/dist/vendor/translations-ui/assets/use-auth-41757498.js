b327ee9e9a9e91cd0c5a161d5e3e4f6987a14097
Simon Roland <simon@uptime.dk>
2025-08-21 21:20:10 +0200

    Build assets

--- resources/dist/vendor/translations-ui/assets/use-auth-41757498.js
+++
@@ -1,1 +0,0 @@
-import{v as r,_ as s}from"./app-fd4fef8f.js";const u=()=>r(()=>s().props.auth.user);export{u};
---
+++ resources/dist/vendor/translations-ui/assets/use-auth-f25af1d5.js
@@ -0,0 +1,1 @@
+import{v as r,_ as s}from"./app-94134e30.js";const u=()=>r(()=>s().props.auth.user);export{u};
