b04193b46bfc921ced8e25b625a32f0220e49df6
Simon Roland <simon@uptime.dk>
2025-08-22 10:47:48 +0200

    Remove indentation from markdown

--- resources/views/mail/invite.blade.php
+++ resources/views/mail/invite.blade.php
@@ -1,16 +1,16 @@
 @component('mail::message')
-    # Hello
+# Hello
 
-    Someone has invited you to join their translation team on {{ config('app.name') }}.
+Someone has invited you to join their translation team on {{ config('app.name') }}.
 
-    @component('mail::button', ['url' => $link])
-        Accept Invitation
-    @endcomponent
+@component('mail::button', ['url' => $link])
+Accept Invitation
+@endcomponent
 
-    This invitation link will expire in 24 hours.
+This invitation link will expire in 24 hours.
 
-    If you did not request a translation invitation, please ignore this e-mail.
+If you did not request a translation invitation, please ignore this e-mail.
 
-    Thanks,  
-    {{ config('app.name') }}
+Thanks,  
+{{ config('app.name') }}
 @endcomponent
