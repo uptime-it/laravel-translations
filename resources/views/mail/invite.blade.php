4665d2c97d4bfb78482c19d349cb467f0507ed06
Simon Roland <simon@uptime.dk>
2025-08-22 10:42:12 +0200

    Fix invitation email wording and formatting

--- resources/views/mail/invite.blade.php
+++ resources/views/mail/invite.blade.php
@@ -1,7 +1,7 @@
 @component('mail::message')
     # Hello
 
-    Someone has invited you to join their  translation team on {{ config('app.name') }}.
+    Someone has invited you to join their translation team on {{ config('app.name') }}.
 
     @component('mail::button', ['url' => $link])
         Accept Invitation
@@ -9,8 +9,8 @@
 
     This invitation link will expire in 24 hours.
 
-    If you did not request a password reset, no further action is required.
+    If you did not request a translation invitation, please ignore this e-mail.
 
-    Thanks,<br>
+    Thanks,  
     {{ config('app.name') }}
 @endcomponent
